939fa17626c19141042794ccd966fb7a8cbbdcf3
danchurch <danchurchthomas@gmail.com>
2022-01-26 10:04:58 +0100

    add station data to usmap

--- js/ch2/fig2.1/proj/fig2.1.js
+++ js/ch2/fig2.1/proj/fig2.1.js
@@ -3,36 +3,44 @@ window.addEventListener("load", main)
 function main(){
 
 // get data. We'll use the project version for now?
-  d3.json('https://gist.githubusercontent.com/mheydt/29eec003a4c0af362d7a/raw/d27d143bd75626647108fc514d8697e0814bf74b/us-states.json').then(function(mapUSA) {
+  d3.json('https://gist.githubusercontent.com/mheydt/29eec003a4c0af362d7a/raw/d27d143bd75626647108fc514d8697e0814bf74b/us-states.json')
+    .then(function(mapUSA) {
 
-    // can we delete alaska, Puerto Rico and hawaii? they don't fit into the range of our data. 
-    let dontWant = ["Hawaii","Alaska","Puerto Rico"];
-    mapUSA.features = mapUSA.features.filter(x => {return(!dontWant.includes(x.properties.name))});
+        // can we delete alaska, Puerto Rico and hawaii? they don't fit into the range of our data. 
+        let dontWant = ["Hawaii","Alaska","Puerto Rico"];
+        mapUSA.features = mapUSA.features.filter(x => {return(!dontWant.includes(x.properties.name))});
 
+        // get our data in there. Let's put it on the github repo to make it more 
+        // accessible
 
-    // svg vars
-    const width = 600, height = 600;
+        let pointData = d3.csv("https://raw.githubusercontent.com/danchurch/bayesSpatioTemp/main/js/ch2/fig2.1/Tmax_1.csv")
 
-    // size of map facets
-    let facetWidth = width; //for now
-    let facetHeight = height; //for now
+        // here we would put in our scatter plot with the lat/long coordinates instead of console.log
+        pointData.then(function(data){console.log(data)})
 
-    // project
-    let projection = d3.geoAlbersUsa();
-    projection.fitSize([facetWidth, facetHeight], mapUSA);
-    let geoGenerator = d3.geoPath()
-    .projection(projection);
+        // svg vars
+        const width = 600, height = 600;
 
-    let svg = d3.select("#plot").append('svg')
-    .style("width", width).style("height", height);
+        // size of map facets
+        let facetWidth = width; //for now
+        let facetHeight = height; //for now
 
-  
-    svg.append('g').selectAll('path')
-    .data(mapUSA.features)
-    .join('path')
-    .attr('d', geoGenerator)
-    .attr('fill', '#ffffff')
-    .attr('stroke', '#000');
+        // project
+        let projection = d3.geoAlbersUsa();
+        projection.fitSize([facetWidth, facetHeight], mapUSA);
+        let geoGenerator = d3.geoPath()
+        .projection(projection);
+
+        let svg = d3.select("#plot").append('svg')
+        .style("width", width).style("height", height);
+
+      
+        svg.append('g').selectAll('path')
+        .data(mapUSA.features)
+        .join('path')
+        .attr('d', geoGenerator)
+        .attr('fill', '#ffffff')
+        .attr('stroke', '#000');
 
   });
 
