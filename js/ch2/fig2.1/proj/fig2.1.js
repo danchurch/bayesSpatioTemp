95de40d2227cb2a660b4520c14594ee36316f40b
danchurch <danchurchthomas@gmail.com>
2022-04-26 10:52:29 +0200

    snail pace, still learning d3js

--- js/ch2/fig2.1/proj/fig2.1.js
+++ js/ch2/fig2.1/proj/fig2.1.js
@@ -12,16 +12,26 @@ function main(){
   // projection and generator
   // let projection = d3.geoAlbersUsa();
   let projection = d3.geoEquirectangular()
-    .center([-80,30])
+    //.center([-80,30])
     .translate([200, 200])
-    .scale(200)
+    .scale(400)
     ;
-
-
   let geoGenerator = d3.geoPath().projection(projection);
+
+  // function to center and scale based on extent of station data:
+  function getCenterExtent(mapCoords) {
+    let allLats = mapCoords.map( d => d.lat );
+    let allLons = mapCoords.map( d => d.lon );
+    let centersLatLon = {"lon":d3.mean(allLons), "lat":d3.mean(allLats)};
+    return(centersLatLon);
+  };
 
   // state drawing function:
   function renderStates(data, data2) {
+    let centerCoords = getCenterExtent(data2); 
+    projection.center([centerCoords.lon, centerCoords.lat]);
+    console.log([centerCoords.lon, centerCoords.lat]);
+    //projection.center([-80,30]);
     svg1.append('g')
     .attr('id','statePaths')
     .selectAll('path')
@@ -29,7 +39,7 @@ function main(){
       .join('path')
       .attr('d', geoGenerator)
       .attr('fill', '#ffffff')
-      .attr('stroke', '#000')
+      .attr('stroke', '#000');
     };
 
   // station plotting function:
@@ -39,7 +49,7 @@ function main(){
       .data(data)
       .enter()
       .append("circle")
-        .attr("r", 5)
+        .attr("r", 2)
         .attr("fill", "red")
         .attr( "cx", d => projection([d.lon, d.lat])[0])
         .attr( "cy", d => projection([d.lon, d.lat])[1]);
@@ -52,7 +62,7 @@ function main(){
       d3.csv("https://raw.githubusercontent.com/danchurch/bayesSpatioTemp/main/js/ch2/fig2.1/Tmax_1.csv")
         .then(function(stationData){
            renderStates(mapUSA, stationData);
-           //drawStations(stationData);
+           drawStations(stationData);
     });
   });
 } // end of main, anything past here is probably a mistake
