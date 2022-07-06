1799e86bdbe0220724d50d70ceafed36616039d4
danchurch <danchurchthomas@gmail.com>
2022-07-06 10:34:15 +0200

    arkansas rendered

--- js/ch2/fig2.4/fig2.4.js
+++ js/ch2/fig2.4/fig2.4.js
@@ -1,17 +1,34 @@
 window.addEventListener("load", main);
 
 function main(){
-  d3.json("MissouriCounties.GeoJSON").then(
+  d3.json("https://raw.githubusercontent.com/danchurch/bayesSpatioTemp/main/dataSets/ch2/MissouriCounties.GeoJSON").then(
     function drawCounties(counties){
+
+        console.log(counties);
+
         const width = 100, height = 100;
-        //let projection = d3.geoEquirectangular()
-        //    .translate([width/2, height/2])
-        //    .scale(1000)
-        //    .center([-92.6, 38.47])
-        //;
+        let projection = d3.geoEquirectangular()
+            .translate([width/2, height/2])
+            .scale(1000)
+            .center([-92.6, 38.47])
+        ;
       
-        //let geogenerator = d3.geoPath().projection(projection); 
+        let geogenerator = d3.geoPath().projection(projection); 
+
+        let svg = d3.select('#plot')
+          .append('svg')
+          .attr('width',width)
+          .attr('height',height)
+          ;      
       
-      
+        let countyPaths = svg.append('g')
+          .attr('id','counties')
+          .selectAll('path')
+            .data(counties.features)
+            .join('path')
+            .attr('d', geogenerator)
+            .attr('fill','#ffffff')
+            .attr('stroke','#000');
+
 });}
 // end of main
