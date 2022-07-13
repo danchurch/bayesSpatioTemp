2b3ab09dfd4194f8e1e71598b844e7b8cedcd2bb
danchurch <danchurchthomas@gmail.com>
2022-07-13 10:55:07 +0200

    debug map, st. louises

--- js/ch2/fig2.4/fig2.4.js
+++ js/ch2/fig2.4/fig2.4.js
@@ -1,19 +1,17 @@
 window.addEventListener("load", main);
 
 function main(){
-  d3.json("https://raw.githubusercontent.com/danchurch/bayesSpatioTemp/main/dataSets/ch2/MissouriCounties.GeoJSON").then(
+  d3.json("https://raw.githubusercontent.com/danchurch/bayesSpatioTemp/main/dataSets/ch2/countiesGeoInc.GeoJSON").then(
     function drawCounties(counties){
 
         console.log(counties);
-
-        //let aa = counties.features.filter(d => d.properties.NAME === "Henry");
-        //let aa = counties.features.map(d => d.properties.NAME);
-        //console.log(aa);
 
         // this also works, if we don't know the entire string:  
         //let bb = counties.features.filter( d => d.NAME == 'St. Louis');
         let bb = counties.features.filter( d => d.properties.NAME.includes('Louis')); 
         console.log(bb);
+        // for some reason that returns two copies of St. Louis county
+        // are there two entries in our data?
 
 
         const width = 100, height = 100;
@@ -34,7 +32,8 @@ function main(){
         let countyPaths = svg.append('g')
           .attr('id','counties')
           .selectAll('path')
-            .data(counties.features)
+            .data(bb) // check the duplicated county
+            //.data(counties.features)
             .join('path')
             .attr('d', geogenerator)
             .attr('fill','#ffffff')
