0b6c7f256d2a9bc05a27675b09657a5efb4c8771
danchurch <danchurchthomas@gmail.com>
2022-07-14 10:37:26 +0200

    blue missouri

--- js/ch2/fig2.4/fig2.4.js
+++ js/ch2/fig2.4/fig2.4.js
@@ -8,10 +8,11 @@ function main(){
 
         // this also works, if we don't know the entire string:  
         //let bb = counties.features.filter( d => d.NAME == 'St. Louis');
-        let bb = counties.features.filter( d => d.properties.NAME.includes('Louis')); 
-        console.log(bb);
+        //let bb = counties.features.filter( d => d.properties.NAME.includes('Louis')); 
+        //console.log(bb);
         // for some reason that returns two copies of St. Louis county
         // are there two entries in our data?
+        // yup. removed the city itself upstream, in python
 
 
         const width = 100, height = 100;
@@ -23,6 +24,11 @@ function main(){
       
         let geogenerator = d3.geoPath().projection(projection); 
 
+        let colScale = d3.scaleLog()
+                           .domain([d3.min(counties.features, d => d.properties.X1970),
+                                    d3.max(counties.features, d => d.properties.X1970)])
+                           .range(['blue','yellow','red']);
+
         let svg = d3.select('#plot')
           .append('svg')
           .attr('width',width)
@@ -32,12 +38,20 @@ function main(){
         let countyPaths = svg.append('g')
           .attr('id','counties')
           .selectAll('path')
-            .data(bb) // check the duplicated county
-            //.data(counties.features)
+            .data(counties.features)
             .join('path')
             .attr('d', geogenerator)
-            .attr('fill','#ffffff')
+            .attr('fill', d => colScale(d.properties.X1970))
+            //.attr('fill','#ffffff')
             .attr('stroke','#000');
+
+        // log color scale
+
+
+
+
+        console.log(colScale([3282]));
+        console.log(colScale([2832]));
 
 });}
 // end of main
