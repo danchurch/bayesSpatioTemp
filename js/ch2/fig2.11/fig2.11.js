dcc726b9cb642fa1960948dbce6ebe11684aa5aa
danchurch <danchurchthomas@gmail.com>
2022-10-10 09:50:17 +0200

    geoMeanStationTemp csv added

--- js/ch2/fig2.11/fig2.11.js
+++ js/ch2/fig2.11/fig2.11.js
@@ -1,77 +1,63 @@
 window.addEventListener("load", main);
 
 function main(){
-d3.csv('https://raw.githubusercontent.com/danchurch/bayesSpatioTemp/main/dataSets/ch2/latitude_hov_heatmapData.csv')
-  .then(function drawHovmuller(data){
-
-
-// sample data:
-
-  exData = [
-            {"x":0, "y":0, "z":1},
-            {"x":0, "y":10, "z":2},
-            {"x":10, "y":0, "z":3},
-            {"x":10, "y":10, "z":4},
-           ];
+  let url = 'https://raw.githubusercontent.com/danchurch/bayesSpatioTemp/main/dataSets/ch2/latitude_hov_heatmapData.csv';
+  d3.csv(url, d3.autoType)
+    .then(function drawHovmuller(data){
   
-  console.log(exData);
-  console.log(data);
-
-  let cnv = d3.select('#plot')
-    .append("canvas")
-    .attr('width', 400).attr('height', 400).attr('id', 'cnv');
+    let [cWidth, cHeight] = [100,100];
   
-  let ctx = cnv.node().getContext('2d');
+    let cnv = d3.select('#plot')
+      .append("canvas")
+      .attr('width', cWidth).attr('height', cHeight).attr('id', 'cnv');
+    
+    let ctx = cnv.node().getContext('2d');
+  
+    // scale for x
+    let xSc = d3.scaleLinear()
+      .domain(d3.extent(data, d => d["x"]))  
+      .range([0,100]);
+    // scale for y
+    let ySc = d3.scaleLinear()
+      .domain(d3.extent(data, d => d["y"]))  
+      .range([0,100]);
+    // scale for colors
+    // linear scale
+    //let zSc = d3.scaleLinear()
+    //  .domain(d3.extent(data, d => d["z"]))  
+    //  .range(["blue","yellow","red"]);
+    // sinebow:
+    let zSc = d3.scaleSequential( t => d3.interpolateSinebow(2/3-3*t/4))
+      .domain(d3.extent(data, d => d["z"]));
+  
+    // scalar for rectangle width
+    [xmin, xmax] = d3.extent(data, d => d["x"]);
+    pWidth = (cWidth - 1)/(xmax - xmin);
+  
+    // scale for rectangle height
+  
+    // make a new data array using these?:
+    let scaleDat = function(oldData){
+      newData = [];
+      newData['x'] = xSc(oldData['x']);
+      newData['y'] = ySc(oldData['y']);
+      newData['z'] = zSc(oldData['z']);
+      return(newData);
+      };
+  
+    // function for drawing a pixel:
+    let drawPix = function(obj){
+      ctx.fillStyle = obj["z"];
+      ctx.fillRect(obj["x"], obj["y"], pWidth, 1);
+    }
+  
+    // scale the data
+    let newdata = data.map(scaleDat);
 
-  // example rectangles
-  //ctx.fillStyle = "Red";
-  //ctx.fillRect(10, 10, 100, 100);
-
-  //ctx.fillStyle = "Blue";
-  //ctx.fillRect(110, 10, 100, 100);
-
-  // scale for colors
-  let zSc = d3.scaleLinear()
-    .domain(d3.extent(exData, d => d["x"]))  
-    .range(["red","white","blue"]);
-
-  // scale for x
-  let xSc = d3.scaleLinear()
-    .domain(d3.extent(data, d => d["x"]))  
-    .range([0,40]);
- 
-  // function for drawing a pixel:
-  let drawPix = function(obj){
-    ctx.fillStyle = zSc(obj["z"]);
-    ctx.fillRect(xSc(obj["x"]), obj["y"], 1, 1);
-  }
-
-  let testLoc = function(obj){
-    colVal = zSc(obj["z"]);
-    [x,y] = [ xSc(obj["x"]), obj["y"], 1, 1];
-    console.log(colVal + ' ' + x + ' ' + y);
-  }
-   
-  //data.forEach(drawPix);
-  //exData.forEach(drawPix);
-
-  exData.forEach(testLoc);
-
-  data.forEach(testLoc);
-
+    // draw it
+    newdata.forEach(drawPix);
 
 })}
 
 // end of main
-
-// so, we need to learn how to do heat maps
-
-// how can we convert a matrix of values to a heat map? 
-
-// general strategy - get data into an array of objects, with three properties: 
-// x, y, z. Write a function that plots a 1-pixel rectangle for each object 
-// in the array.  
-
-// Janert then uses nested for loops, but couldn't we do this with "forEach()"?
-
-// an svg solution is here: https://stackoverflow.com/questions/66661192/d3-heatmap-syntax
+// an svg solutioN IS here: https://stackoverflow.com/questions/66661192/d3-heatmap-syntax
